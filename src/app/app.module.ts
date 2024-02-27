b37dd5075c40c075244f70b5913e2d6616347e0d
AlexLeoChumak <chumak1106@gmail.com>
2024-02-27 13:54:56 +0300

    setting up firebase & font awesome connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { FirestoreModule } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { environment } from 'src/environments/environment.prod';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    FirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBJFNBGFNBnDDLbynSi8cGn18OHjZng8ro',
+    authDomain: 'arrt-d1c5a.firebaseapp.com',
+    projectId: 'arrt-d1c5a',
+    storageBucket: 'arrt-d1c5a.appspot.com',
+    messagingSenderId: '836020465072',
+    appId: '1:836020465072:web:4ab30a3519980a956b3a75',
+  },
+};

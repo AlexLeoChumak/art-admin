a58d08b2d146b39af94f33148d2b8422562c45e4
AlexLeoChumak <chumak1106@gmail.com>
2024-03-31 17:33:11 +0300

    debug

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,18 +1,16 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   Router,
 } from '@angular/router';
-import { Observable, Subscription, catchError, map, throwError } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  checkAuthSub!: Subscription;
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
